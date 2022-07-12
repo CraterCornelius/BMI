c5c9d20a72dcc23d1611d96a94cf4fd487c0c3b8
CraterCornelius <la.trompette.des.morts@gmail.com>
2022-07-12 16:15:51 +0300

    Code formated+Correct class name

--- src/BMIservice.java
+++
@@ -1,8 +0,0 @@
-public class BMIservice {
-
-    public double calculate (double height , int weight){
-        double result = weight / (height * height) ;
-
-        return result;
-    }
-}
---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+
+    public double calculate(double height, int weight) {
+        double result = weight / (height * height);
+
+        return result;
+    }
+}
